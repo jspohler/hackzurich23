0b9e866ae3d44803e1a75a2c971fd118eca97ca1
JakobSpohler <jspohler@posteo.de>
2023-09-16 09:23:01 +0200

    xml and html merged into vovas files

--- file_reader.py
+++ file_reader.py
@@ -1,7 +1,19 @@
 import sqlite3
 import os
 import glob
+import re
 from docx import Document
+
+def open_file_with_encodings(file_path):
+    encodings_to_try = ['utf-8', 'latin-1', 'utf-16']
+    for encoding in encodings_to_try:
+        try:
+            with open(file_path, 'r', encoding=encoding) as file:
+                content = file.read()
+            return content, encoding  # Return the content if successfully read
+        except UnicodeDecodeError:
+            pass
+    return None  # Return None if all encodings fail
 
 
 def extract_data_from_db(db_file, output_folder):
@@ -88,19 +100,64 @@ def extract_log_data(folder_path, output_folder):
     
 folder_path = 'files'
 
+def extract_xml_data(folder_path, output_folder):
+    try:
+        os.makedirs(output_folder, exist_ok=True)
+
+        xml_files = glob.glob(os.path.join(folder_path, '*.xml'))
+        for xml_file in xml_files:
+            output_file_path = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(xml_file))[0]}_xml.txt")
+            content, encoding = open_file_with_encodings(xml_file)
+
+            # Remove XML tags and content
+            content_no_xml = re.sub(r'<[^>]+>', '', content)
+
+            # Remove consecutive empty lines
+            content_cleaned = re.sub(r'\n\s*\n', '\n', content_no_xml)
+
+            with open(output_file_path, 'w', encoding=encoding) as output_file:
+                output_file.write(content_cleaned)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def extract_html_data(folder_path, output_folder):
+    try:
+        os.makedirs(output_folder, exist_ok=True)
+
+        html_files = glob.glob(os.path.join(folder_path, '*.html'))
+        for html_file in html_files:
+            output_file_path = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(xml_file))[0]}_html.txt")
+            content, encoding = open_file_with_encodings(html_file)
+            content_no_xml = re.sub(r'<[^>]+>', '', content)
+            content_cleaned = re.sub(r'\n\s*\n', '\n', content_no_xml)
+
+            with open(output_file_path, 'w', encoding=encoding) as output_file:
+                output_file.write(content_cleaned)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 #log
 if True:
-    output_folder_log = 'texted_from_files/log/'
+    output_folder_log =  'texted_from_files/log/'
     extract_log_data(folder_path, output_folder_log)
 
 
 #docx
-if False:
+if True:
     output_folder_docx = 'texted_from_files/docx/'
     extract_docx_data(folder_path, output_folder_docx)
     
 #db
-if False:
+if True:
     output_folder_db = 'texted_from_files/db/'
     extract_db_data(folder_path, output_folder_db)
 
+#xml
+if True:
+    output_folder_xml = 'texted_from_files/xml/'
+    extract_xml_data(folder_path,output_folder_xml)
+
+#html
+if True:
+    output_folder_html = 'texted_from_files/html/'
+    extract_xml_data(folder_path,output_folder_html)
